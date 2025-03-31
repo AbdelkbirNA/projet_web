021abb3054e9010b9379bf00e027becf8024b358
Abdelkbir Nainiaa <abdlekbir.nainiaa12@gmail.com>
2025-03-31 02:05:26 +0000

    gin

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,9 @@ Route::get('/', function () {
     return view('Ensiasd.Main'); // Assure-toi que la structure respecte bien `resources/views/Ensiasd/Main.blade.php`
 })->name('home');
 
-Route::get('/professors', [ProfessorController::class, 'index'])->name('professors');
-
+Route::get('/professors', function () {
+    return view('Ensiasd.professors');
+});
 
 Route::get('/prof', function () {
     return view('prof');
