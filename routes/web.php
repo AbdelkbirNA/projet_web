d637280ad3a20853050c5d6d5dc339a7d8a3b147
zineb-bd <2003zinebboughedda@gmail.com>
2025-05-21 10:43:30 +0100

    web jdida

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,8 @@ use Illuminate\Support\Facades\Response;
 use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\ForumController;
 use App\Http\Controllers\AnswerController;
+use App\Http\Controllers\ContactController; // Ajouté
+
 use App\Models\Profile;
 
 // Routes d'authentification personnalisées
@@ -21,6 +23,7 @@ Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
 Route::post('/register', [RegisterController::class, 'register']);
 Route::get('/professor/{id}', [ProfileController::class, 'showProfessor'])->name('professor.show');
+
 // Page d'accueil
 Route::get('/', function () {
     $professors = Profile::whereHas('user', function($query) {
@@ -36,58 +39,56 @@ Route::get('/abdo', function () {
 
 // Pages publiques accessibles à tous
 Route::get('/professors', [ProfileController::class, 'index'])->name('professors');
-Route::get('/professor/{id}', [ProfileController::class, 'showProfessor'])->name('professor.show');
-
-// Route spécifique pour Prof Abdo (tu peux en ajouter d'autres pour chaque prof)
 Route::get('/professors/abdo', function () {
     return view('professor.Abdo');
 })->name('professor.abdo');
-
-// Exemple de route dynamique si tu veux généraliser (optionnel)
-// Route::get('/professors/{slug}', [ProfessorController::class, 'show'])->name('professor.show');
 
 // Route pour afficher un profil depuis la base (si besoin)
 Route::get('/professor/{id}', [ProfileController::class, 'showProfessor'])->name('professor.show');
 
 // Groupe de routes protégées
 Route::middleware(['auth'])->group(function () {
+
     // Routes pour les professeurs
     Route::middleware(['user.type:professor'])->group(function () {
         Route::get('/homeProf', function () {
             return view('professor.publications.acceuil');
         })->name('prof_main');
-        
+
         Route::get('/professor/publications', [PublicationController::class, 'professorIndex'])
             ->name('professor.publications.index');
     });
-    
+
     // Routes pour les étudiants
     Route::middleware(['user.type:student'])->group(function () {
         Route::get('/homeStudent', function () {
             return view('student.publications.acceuil');
         })->name('student_main');
-        
+
         Route::get('/student/publications', [PublicationController::class, 'studentIndex'])
             ->name('student.publications.index');
-            
+
         Route::get('/student/professors/{professor}/publications', [StudentController::class, 'showProfessorPublications'])
             ->name('student.professor.publications');
+
+        // Routes pour le contact (Ajouté)
+        Route::get('/contact', [ContactController::class, 'show'])->name('contact.show');
+        Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
     });
 
-Route::get('/about/{id}', [ProfileController::class, 'showAbout'])->name('profile.about');    Route::get('/profile/create', [ProfileController::class, 'create'])->name('profile.create');
+    // Profil
+    Route::get('/about/{id}', [ProfileController::class, 'showAbout'])->name('profile.about'); // Correction du nom
+    Route::get('/profile/create', [ProfileController::class, 'create'])->name('profile.create');
     Route::post('/profile', [ProfileController::class, 'store'])->name('profile.store');
     Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
-
-    // Route pour la mise à jour de la photo de profil
+    Route::put('/profile/update', [ProfileController::class, 'update'])->name('profile.update');
     Route::put('/profile/{id}/photo', [ProfileController::class, 'updatePhoto'])->name('profile.updatePhoto');
-    // Route pour la mise à jour des infos de contact
     Route::put('/profile/{id}/contact', [ProfileController::class, 'updateContact'])->name('profile.updateContact');
 
-    // Route pour la mise à jour du profil
-    Route::put('/profile/update', [ProfileController::class, 'update'])->name('profile.update');
-
+    // Cours
     Route::resource('courses', CourseController::class);
 
+    // Téléchargement & consultation de ressources
     Route::get('/courses/resources/{filename}', function ($filename) {
         $path = storage_path('app/resources/' . $filename);
 
@@ -102,7 +103,7 @@ Route::get('/about/{id}', [ProfileController::class, 'showAbout'])->name('profil
             'Content-Type' => $type,
             'Content-Disposition' => 'inline; filename="' . $filename . '"'
         ]);
-    })->name('courses.resources.view');
+    })->name('courses.resources.view'); // Ajouté
 
     Route::get('/courses/resources/download/{filename}', function ($filename) {
         $path = storage_path('app/resources/' . $filename);
@@ -116,22 +117,9 @@ Route::get('/about/{id}', [ProfileController::class, 'showAbout'])->name('profil
         return Response::download($path, $filename, [
             'Content-Type' => $type,
         ]);
-    })->name('courses.resources.download');
-    
-Route::get('/professor/{id}/courses', [CourseController::class, 'professorCourses'])->name('professor.courses');
-Route::get('/professor/{id}/courses', [CourseController::class, 'professorCourses'])->name('professor.courses');
-    // Professeur : gestion questions
-    Route::get('/courses/{course}/questions', [QuestionController::class, 'index'])->name('questions.index');
-    Route::get('/courses/{course}/questions/create', [QuestionController::class, 'create'])->name('questions.create');
-    Route::post('/courses/{course}/questions', [QuestionController::class, 'store'])->name('questions.store');
-    Route::get('/questions/{question}/edit', [QuestionController::class, 'edit'])->name('questions.edit');
-    Route::put('/questions/{question}', [QuestionController::class, 'update'])->name('questions.update');
-    Route::delete('/questions/{question}', [QuestionController::class, 'destroy'])->name('questions.destroy');
+    })->name('courses.resources.download'); // Ajouté
 
-    // Étudiant : accès forum + réponses
-    Route::get('/courses/{course}/forum', [ForumController::class, 'show'])->name('forum.show');
-    Route::post('/questions/{question}/answers', [AnswerController::class, 'store'])->name('answers.store');
-
+    // Alternative avec contrôleur (ajoutée dans le premier code)
     Route::get('courses/resources/view/{filename}', [CourseController::class, 'viewResource'])
         ->where('filename', '.*')
         ->name('courses.resources.view');
@@ -139,4 +127,18 @@ Route::get('/professor/{id}/courses', [CourseController::class, 'professorCourse
     Route::get('courses/resources/download/{filename}', [CourseController::class, 'downloadResource'])
         ->where('filename', '.*')
         ->name('courses.resources.download');
+
+    // Cours professeur
+    Route::get('/professor/{id}/courses', [CourseController::class, 'professorCourses'])->name('professor.courses');
+
+    // Questions & Réponses
+    Route::get('/courses/{course}/questions', [QuestionController::class, 'index'])->name('questions.index');
+    Route::get('/courses/{course}/questions/create', [QuestionController::class, 'create'])->name('questions.create');
+    Route::post('/courses/{course}/questions', [QuestionController::class, 'store'])->name('questions.store');
+    Route::get('/questions/{question}/edit', [QuestionController::class, 'edit'])->name('questions.edit');
+    Route::put('/questions/{question}', [QuestionController::class, 'update'])->name('questions.update');
+    Route::delete('/questions/{question}', [QuestionController::class, 'destroy'])->name('questions.destroy');
+
+    Route::get('/courses/{course}/forum', [ForumController::class, 'show'])->name('forum.show');
+    Route::post('/questions/{question}/answers', [AnswerController::class, 'store'])->name('answers.store');
 });
